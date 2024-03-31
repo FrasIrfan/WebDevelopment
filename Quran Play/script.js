202313b580aa950d2c3c534e902e2a76c0d14efc
FrasIrfan <leofras0051@gmail.com>
2024-03-31 23:49:04 +0500

    extended the qari and surah list

--- Quran Play/script.js
+++ Quran Play/script.js
@@ -4,31 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const audioPlayer = document.getElementById("audio-player");
   const bookmarkBtn = document.getElementById("bookmark-btn");
 
-  // Simulated qari data
   const qariData = [
     { name: "Abdul Basit", identifier: "qari1" },
     { name: "Abdul Samad", identifier: "qari2" },
-    // Add more qari data as needed
+    { name: "Abdallah Humeid", identifier: "qari3" },
+    { name: "Abdul Azeez Ar Rawdhaan", identifier: "qari4" },
+    { name: "Abdul Majid Rahman", identifier: "qari5" },
+    { name: "Abdul Kabeer Haidari", identifier: "qari6" },
+    { name: "Abdulhadi Kanakeri", identifier: "qari7" },
+    { name: "Abdulkabir Al Hadidi", identifier: "qari8" },
+    { name: "Abdullah Al Buajan", identifier: "qari9" },
+    { name: "Abdur Rahman As-Sudais", identifier: "qari10" },
   ];
 
-  // Simulated surah data
-  const surahData = {
-    qari1: [
-      { name: "Surah AlFatiha", number: 1, audio: "audio/surah_Fatiha.mp3" },
-      { name: "Surah AlBaqara", number: 2, audio: "audio/surah2.mp3" },
-      // Add more surah data for qari1
-    ],
-    qari2: [
-      { name: "Surah 3", number: 3, audio: "" },
-      { name: "Surah 4", number: 4, audio: "surah4.mp3" },
-      // Add more surah data for qari2
-    ],
-  };
+  const surahData = [
+    { name: "Surah AlFatiha", number: 1, audio: "audio/surah_Fatiha.mp3" },
+    { name: "Surah AlBaqara", number: 2, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 3, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 4, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 5, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 6, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 7, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 8, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 9, audio: "audio/surah.mp3" },
+    { name: "Surah AlBaqara", number: 10, audio: "audio/surah2.mp3" },
+  ];
 
-  // Function to play selected surah
   function playSurah(qariIdentifier, surahNumber) {
-    const surahs = surahData[qariIdentifier];
-    const surah = surahs.find((surah) => surah.number == surahNumber);
+    const surah = surahData.find((surah) => surah.number == surahNumber);
     if (surah && surah.audio) {
       audioPlayer.src = surah.audio;
       audioPlayer.play();
@@ -37,7 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Function to populate qari select options
   function populateQariSelect() {
     qariData.forEach((qari) => {
       const option = document.createElement("option");
@@ -47,11 +49,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Function to fetch surahs based on selected qari
   function fetchSurahs(qariIdentifier) {
     surahSelect.innerHTML = "";
-    const surahs = surahData[qariIdentifier];
-    surahs.forEach((surah) => {
+    surahData.forEach((surah) => {
       const option = document.createElement("option");
       option.text = surah.name;
       option.value = surah.number;
@@ -73,19 +73,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   toggleSwitch.addEventListener("change", switchTheme);
 
-  // Populate qari select options on page load
   populateQariSelect();
 
-  // Event listener for qari selection
   qariSelect.addEventListener("change", function () {
     const selectedQari = this.value;
     fetchSurahs(selectedQari);
   });
 
-  // Event listener for surah selection
   surahSelect.addEventListener("change", function () {
-    const selectedQari = qariSelect.value;
     const selectedSurah = this.value;
+    const selectedQari = qariSelect.value;
     playSurah(selectedQari, selectedSurah);
   });
 });
