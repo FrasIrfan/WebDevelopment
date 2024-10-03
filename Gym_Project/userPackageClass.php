00077e6339c38a99c1e5a5466ecc3359385e95f9
FrasIrfan <leofras0051@gmail.com>
2024-10-03 13:44:03 +0500

    feature: Add functionality to fetch current package for user
    - Created UserPackage class to manage user package-related operations.
    - Implemented getCurrentPackage method to retrieve the current package's name and price based on the user ID.

---
+++ Gym_Project/userPackageClass.php
@@ -0,0 +1,28 @@
+<?php
+
+class UserPackage
+{
+    private $db;
+    private $userID;
+
+    public function __construct(Database $db, $userID)
+    {
+        $this->db = $db;
+        $this->userID = $userID;
+    }
+
+    public function getCurrentPackage()
+    {
+        $sql = "
+            SELECT P1.PackageName AS CurrentPackageName, P1.PackagePrice AS CurrentPackagePrice
+            FROM UserPackage UP
+            JOIN Packages P1 ON UP.PackageID = P1.PackageID
+            WHERE UP.UserID = ?
+        ";
+
+        $result = $this->db->query($sql, [$this->userID]);
+
+        return $result ? $result[0] : null; // Return the first row or null if no package
+    }
+}
+?>
