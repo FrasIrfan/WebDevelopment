bd13e7bc23e732ce2a0a5ca07250c1373cab97c2
FrasIrfan <leofras0051@gmail.com>
2024-10-07 01:40:25 +0500

    feat: linked external CSS for background image and button styling
    - Added external CSS file  for enhanced layout and design.
    - Integrated a background image and customized styles.

--- Gym_Project/markAttendance.php
+++ Gym_Project/markAttendance.php
@@ -61,6 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mark Attendance</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="info.css">
 </head>
 
 <body>
