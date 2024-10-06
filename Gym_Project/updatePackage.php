0c15b81d5527baf741ca29e7f5f5a2c3e9117329
FrasIrfan <leofras0051@gmail.com>
2024-10-07 01:41:57 +0500

    feat: linked external CSS for background image, button styling and input fields
    - Added external CSS file  for enhanced layout and design.
    - Integrated a background image and customized styles.

--- Gym_Project/updatePackage.php
+++ Gym_Project/updatePackage.php
@@ -29,6 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Packages</title>
     <!-- Including bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="info.css">
+
 </head>
 
 <body>
